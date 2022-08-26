4637eec820a09e0316661127abc9dd3c6ec88128
Aaron Stannard <aaron@petabridge.com>
2022-08-26 13:52:07 -0500

    initial commit

---
+++ src/SystemMemorySerializer/Serializer.cs
@@ -0,0 +1,22 @@
+﻿using System.Buffers;
+
+namespace SystemMemorySerializer
+{
+    public abstract class Serializer
+    {
+        protected Serializer(short identifier)
+        {
+            Identifier = identifier;
+        }
+
+        public virtual short Identifier { get; }
+        
+        public abstract bool IncludeManifest { get; }
+
+        public abstract IMemoryOwner<byte> ToBinary(object obj);
+
+        public abstract string ToManifest(object obj);
+
+        public abstract object FromBinary(IMemoryOwner<byte> memory, string manifest);
+    }
+}
